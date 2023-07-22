5dbf057a8a72895a762008ea45e9d8d72b158b34
Vagif Aliev <kritohanzo0@gmail.com>
2023-07-23 00:32:42 +0500

    add allowed_hosts

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = (
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foodgram.kritohanzo.ru', 'kritohanzo.ru', '158.160.73.82']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
